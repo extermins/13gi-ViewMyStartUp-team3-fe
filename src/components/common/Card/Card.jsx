986cd9ed1d8e83f1892ba69416407a1867b6651a
seoyoon-k <its.yoon.work@gmail.com>
2026-05-22 17:37:18 +0900

    feat: Card, List component 추가

---
+++ src/components/common/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import icon from "../../../assets/icons/ic-minus.svg";
+import styles from "./Card.module.css";
+
+const Card = ({ imageUrl, title, subtle }) => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.icon}>
+        <img src={icon} alt="선택 해제" />
+      </div>
+      <div className={styles.info}>
+        <img className={styles.image} src={imageUrl} alt={title}></img>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.subtle}>{subtle}</p>
+      </div>
+    </section>
+  );
+};
+
+export default Card;
---
+++ src/components/common/List/List.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./List.module.css";
+import Button from "../Button/Button.jsx";
+
+const List = ({ imageUrl, title, subtle, label }) => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.info}>
+        <img className={styles.image} src={imageUrl} alt={title}></img>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.subtle}>{subtle}</p>
+      </div>
+      <Button>{label}</Button>
+    </section>
+  );
+};
+
+export default List;
