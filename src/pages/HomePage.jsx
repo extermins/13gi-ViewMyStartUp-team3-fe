3f7917eb6c780a1c1f3e93314b563cd46fda5e8b
apple <apple@appleui-MacBookAir.local>
2026-06-01 16:41:39 +0900

    공용컴포넌트 내 search, dropdown 활용하여 페이지 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+// src/pages/HomePage.jsx
 import React, { useState, useEffect } from "react";
 import ListFilter from "../components/ListFilter/ListFilter";
 import StartupTable from "../components/StartupTable/StartupTable";
