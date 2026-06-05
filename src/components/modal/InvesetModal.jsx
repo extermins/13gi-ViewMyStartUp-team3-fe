ae9348780d560c0a9adf31a367f58e118604bc91
bangminjae <abab1070@naver.com>
2026-06-06 01:22:23 +0900

    fix 버셀 수정4

--- src/components/modal/InvesetModal.jsx
+++ src/components/modal/InvesetModal.jsx
@@ -5,7 +5,7 @@ import { useModal } from "../../hooks/useModal";
 
 import { Input, PasswordInput, TextArea } from "../common/inputfield/Input";
 import Modal from "./Modal";
-import Button from "../common/button/Button";
+import Button from "../common/button/button";
 import corpinvestAPI from "../../pages/companypage/CorpInvestAPI";
 
 export default function InvesetModal({
